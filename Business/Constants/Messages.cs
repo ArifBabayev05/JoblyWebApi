3415b89fb4812c71163180ab2a15746372732541
imac <imac@imacs-MacBook-Pro.local>
2022-08-29 12:38:53 +0400

    Create Login & Register with JWT

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -3,9 +3,15 @@ namespace Business.Constants
 {
     public static class Messages
     {
-        public static string Added = "Uğurla Əlavə Edildi";
-        public static string Deleted = "Uğurla Silindi";
-        public static string Updated = "Uğurla Yeniləndi";
+        public static string Added = "Uğurla Əlavə Edildi!";
+        public static string Deleted = "Uğurla Silindi!";
+        public static string Updated = "Uğurla Yeniləndi!";
+        public static string UserNotFound = "Istifadəçi tapılmadı!";
+        public static string PasswordError = "Şifrə Yanlışdır!";
+        public static string SuccessfulLogin = "Sistemə giriş uğurludur!";
+        public static string UserAlreadyExist = "Istifadəçi zatən mövcuddur!";
+        public static string UserRegistered = "Istifadəçi qeydiyyatdan keçdi!";
+        
 
     }
 }
